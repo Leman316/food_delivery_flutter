cd32141863d29a523096ef7324da439069b8180c
Leman316 <56719469+Leman316@users.noreply.github.com>
2021-03-10 14:12:16 +0600

    Cart Body v1

---
+++ lib/widgets/cartbody.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/models/fooditem.dart';
+import 'package:food_delivery/widgets/cartListItem.dart';
+import 'package:food_delivery/widgets/cart_screen_appbar.dart';
+
+class CartBody extends StatelessWidget {
+  final List<FoodItem> fooditems;
+  CartBody(this.fooditems);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(25, 40, 25, 0),
+      child: Column(
+        children: <Widget>[
+          CartAppBar(),
+          titlebar(),
+          Expanded(
+            child: fooditems.length > 0 ? foodItemList() : noItemContainer(),
+            flex: 1,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget foodItemList() {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return CartListItem(fooditems[index]);
+      },
+      itemCount: fooditems.length,
+    );
+  }
+
+  Widget titlebar() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 35),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "My",
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 35,
+                ),
+              ),
+              Text(
+                "Order",
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 35,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget noItemContainer() {
+    return Container(
+        child: Center(
+            child: Text(
+      'No items in cart',
+      style: TextStyle(
+        fontWeight: FontWeight.w700,
+        fontSize: 20,
+        color: Colors.grey[400],
+      ),
+    )));
+  }
+}
