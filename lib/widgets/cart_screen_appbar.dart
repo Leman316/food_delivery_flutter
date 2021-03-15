86b5173d5bb57ee06e56d28a294c1cabdafa5cc8
Leman316 <56719469+Leman316@users.noreply.github.com>
2021-03-15 18:57:09 +0600

    Drag and Drop V2

--- lib/widgets/cart_screen_appbar.dart
+++ lib/widgets/cart_screen_appbar.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/bloc/cartlistbloc.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:food_delivery/bloc/liststyleColorbloc.dart';
+import 'package:food_delivery/models/fooditem.dart';
 
 class CartAppBar extends StatelessWidget {
   final CartListbloc bloc = BlocProvider.getBloc<CartListbloc>();
@@ -24,8 +26,46 @@ class CartAppBar extends StatelessWidget {
             },
           ),
         ),
+        DragTargetWidget(),
         //  DragTargetWidget(bloc),
       ],
+    );
+  }
+}
+
+class DragTargetWidget extends StatefulWidget {
+  @override
+  _DragTargetWidgetState createState() => _DragTargetWidgetState();
+}
+
+class _DragTargetWidgetState extends State<DragTargetWidget> {
+  final CartListbloc bloc = BlocProvider.getBloc<CartListbloc>();
+  final ColorBloc colorBloc = BlocProvider.getBloc<ColorBloc>();
+
+  @override
+  Widget build(BuildContext context) {
+    return DragTarget<FoodItem>(
+      onWillAccept: (FoodItem foodItem) {
+        colorBloc.setColor(Colors.red);
+        return true;
+      },
+
+      onAccept: (FoodItem foodItem) {
+        colorBloc.setColor(Colors.white);
+
+        bloc.removefromList(foodItem);
+      },
+      //onLeave: (FoodItem foodItem){
+      //colorBloc.setColor(Colors.white); }
+      builder: (context, candidateData, rejectedData) {
+        return Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Icon(
+            CupertinoIcons.delete,
+            size: 30,
+          ),
+        );
+      },
     );
   }
 }
